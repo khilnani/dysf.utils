7e075a609fd3cc52154e5d6fe02fcf6dffc04a97
dysf <info@dysf.co>
2012-12-25 13:16:21 -0500

    Added mock.js for response faking, more tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+//-------------------------------------------------
 
 var log = require("../lib/dysf.utils").logger;
 
@@ -10,4 +11,26 @@ log.event("event");
 log.info("info");
 log.debug("debug");
 log.trace("trace");
+log.trace( { obj: "data"} );
 
+
+//-------------------------------------------------
+
+var utils = require("../lib/dysf.utils").utils;
+var mock = require("../lib/dysf.utils").mock;
+
+// Replace this >> %replace%
+
+var replaceParams = {'%replace%': "REPLACED!!" };
+
+utils.renderFile(mock.response, __filename, replaceParams, function (err) {
+	if(err) 
+  	{
+  		log.error("utils.renderFile");
+  		log.error(err);
+	} else {
+		log.event("utils.renderFile completed.");  	
+	}
+  });
+  		
+  		
